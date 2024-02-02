ccf5767b75562c459ead0a34e179e0e73d4c3761
candiceT233 <candicet233@gmail.com>
2024-02-02 10:29:21 -0600

    move task order pasing to stat_loader

--- flow_analysis/utils/stat_loader.py
+++ flow_analysis/utils/stat_loader.py
@@ -11,7 +11,7 @@ def remove_unwanted_tasks(map_dict, task_list):
             map_dict.pop(task)
     return map_dict
 
-def search_files_with_name(directory, pattern):
+def find_files_with_pattern(directory, pattern):
     file_list = []
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -67,3 +67,37 @@ def load_task_file_map(stat_path, test_name,task_list):
     remove_unwanted_tasks(task_file_map, task_list)
     
     return task_file_map
+
+def load_task_order_list(stat_path):
+    with open(f"{stat_path}/task_order_list.yaml", 'r') as stream:
+        try:
+            task_order_list = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    
+    # switch dict key and value
+    task_order_list = {v: k for k, v in task_order_list.items()}
+    return task_order_list
+
+
+def correct_end_stage(TOL,select_end):
+    # check if SELECT_STAGE_END is in TASK_ORDER_LIST
+    max_order = max(TOL.values())
+    if select_end > max_order:
+        # raise ValueError("SELECT_STAGE_END is not in TASK_ORDER_LIST")
+        select_end = max_order
+        print("STAGE_END is not in TASK_ORDER_LIST, set to max order: {}".format(max_order))
+    return select_end
+
+
+def current_task_order_list(all_TOL, select_start, actual_end):
+    task_list = []
+    for task, order in all_TOL.items():
+        if actual_end == -1:
+            task_list = list(all_TOL.keys())
+        else:
+            if order <= actual_end and order >= select_start:
+                task_list.append(task)
+    # remove keys that are not in task_list
+    new_TOL = {k: v for k, v in all_TOL.items() if k in task_list}
+    return new_TOL
